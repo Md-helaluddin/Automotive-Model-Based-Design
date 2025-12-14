26b34e7ad779dd64e475d64283ddf334b4c6aa39
Md-helaluddin <md-helal.uddin@stud.hshl.de>
2025-12-14 05:06:30 +0100

    speed control

---
+++ Controlling_Speed_PIDConstant/Closed_Loop_with_PID.m
@@ -0,0 +1,36 @@
+% Master_Setup.m
+% Run this to load ALL variables and open the model
+
+clear; clc;
+
+% --- 1. Define Battery Parameters ---
+V_in = 12;        % Battery Voltage (Volts)
+R_bat = 0.5;      % Internal Resistance (Ohms)
+Capacity = 5;     % Battery Capacity (Ah)
+
+% --- 2. Define Motor Parameters ---
+R = 1;            % Resistance
+L = 0.5;          % Inductance (Optional)
+K = 0.01;         % Motor Constant
+J = 0.01;         % Inertia
+b = 0.1;          % Damping
+% --- Gearbox Parameters ---
+G_ratio = 10;     % Gear Ratio
+
+% --- Vehicle Parameters ---
+r_tire = 0.3;     % Tire Radius (0.3 meters)
+m_car = 1000;     % Car Mass (1000 kg)
+rho = 1.225;      % Air Density (how thick the air is)
+Cd = 0.3;         % Aerodynamic Drag (how streamlined the car is)
+Area = 2.5;       % Frontal Area (how big the front of the car is)
+
+disp('SUCCESS: All Battery and Motor variables loaded!');
+
+% --- 3. Open the Simulink Model ---
+% We use 'Motor_Test.slx' because that is where your full circuit is now.
+if isfile('Closed_WithConstant.slx')
+    open_system('Closed_WithConstant.slx');
+    disp('Simulink model opened.');
+else
+    disp('Error: Could not find the Closed_WithConstant');
+end
